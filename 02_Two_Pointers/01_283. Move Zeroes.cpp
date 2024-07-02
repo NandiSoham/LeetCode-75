fe81a3647b28ad6fc5309eb12f59fccd0709d839
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-07-01 23:33:17 -0700

    283. Move Zeroes (Approach -2) -> Solved in C++

--- 02_Two_Pointers/01_283. Move Zeroes.cpp
+++ 02_Two_Pointers/01_283. Move Zeroes.cpp
@@ -26,3 +26,37 @@ public:
 
 // Time Complexity -> O(n)
 // Space Complexity -> O(n)
+
+// ------------------------------------------------------------------------
+
+// ------------------------------- Approach - 2 ---------------------------
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = -1;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == 0){
+                j = i;
+                break;
+            }
+        }
+
+        if(j == -1) return;
+
+        for(int i = j + 1; i < nums.size(); i++){
+            if(nums[i] != 0){
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+
+// ------------------------------------------------------------------------
